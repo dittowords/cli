580a4ca61bf79b68ff3b5ab21b2e8879e51e1cd1
jason@dittowords.com <jason@dittowords.com>
2022-06-27 13:04:39 -0700

    Moves api & output to ts

--- lib/api.ts
+++ lib/api.ts
@@ -3,16 +3,14 @@ const axios = require("axios").default;
 const config = require("./config");
 const consts = require("./consts");
 
-function create(token) {
+export const create = (token: string) => {
   return axios.create({
     baseURL: consts.API_HOST,
     headers: {
       Authorization: `token ${token}`,
     },
   });
-}
+};
 
 module.exports = { create };
-module.exports.default = create(
-  config.getToken(consts.CONFIG_FILE, consts.API_HOST)
-);
+export default create(config.getToken(consts.CONFIG_FILE, consts.API_HOST));
---
+++ lib/output.ts
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+
+export const errorText = (msg: string) => chalk.magenta(msg);
+export const warnText = (msg: string) => chalk.yellow(msg);
+export const info = (msg: string) => chalk.blueBright(msg);
+export const success = (msg: string) => chalk.green(msg);
+export const url = (msg: string) => chalk.blueBright.underline(msg);
+export const subtle = (msg: string) => chalk.grey(msg);
+export const write = (msg: string) => chalk.white(msg);
+export const nl = () => console.log("\n");

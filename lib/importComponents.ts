85886a3e951d216157e838f0bfb0079be9a27bd0
asnewman <ashznewman@gmail.com>
2023-06-13 07:40:58 -0700

    Add flag check for csv files

--- lib/importComponents.ts
+++ lib/importComponents.ts
@@ -9,6 +9,14 @@ async function importComponents(
     csvColumnMapping?: CsvColumnMapping;
   }
 ) {
+  if (
+    filePath.endsWith(".csv") &&
+    (!options.csvColumnMapping?.name || !options.csvColumnMapping?.text)
+  ) {
+    throw new Error(
+      ".csv files require the --component-name and --text flags."
+    );
+  }
   const importResult = await importComponentsHttp(filePath, options);
   console.log(JSON.stringify(importResult));
 }

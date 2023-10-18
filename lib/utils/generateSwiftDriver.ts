61c83aa8f6919ce83b59f024e630336d4e584ed4
Jordin Gardner <gard.jordin@gmail.com>
2023-10-17 22:19:24 -0700

    support for nuanced CLI config

--- lib/utils/generateSwiftDriver.ts
+++ lib/utils/generateSwiftDriver.ts
@@ -9,26 +9,37 @@ interface IArg {
   components?:
     | boolean
     | {
-        root?: boolean | undefined;
-        folders?: string[] | undefined;
+        root?: boolean | { status?: string } | undefined;
+        folders?:
+          | string[]
+          | { id: string | null; status?: string }[]
+          | undefined;
       }
     | undefined;
-  projects?: string[] | undefined;
+  projects?: string[] | { id: string; status?: string }[] | undefined;
 }
 
 export async function generateSwiftDriver(source: SourceInformation) {
   const client = createApiClient();
 
   const body: IArg = {};
-  // TODO: need to add support for nuanced component config, including filtering by status
-  if (source.shouldFetchComponentLibrary) body.components = true;
-  if (source.validProjects)
-    body.projects = source.validProjects.map(({ id }) => id);
+
+  if (source.componentFolders || source.componentRoot) {
+    body.components = {};
+    if (source.componentFolders) {
+      body.components.folders = source.componentFolders;
+    }
+    if (source.componentRoot) {
+      body.components.root = source.componentRoot;
+    }
+  } else if (source.shouldFetchComponentLibrary) {
+    body.components = true;
+  }
+
+  if (source.validProjects) body.projects = source.validProjects;
 
   const { data } = await client.post<string>("/v1/export/swift-driver", body);
-
   const filePath = path.join(consts.TEXT_DIR, "Ditto.swift");
-
   await writeFile(filePath, data);
 
   return `Successfully saved Swift driver to ${output.info("Ditto.swift")}`;

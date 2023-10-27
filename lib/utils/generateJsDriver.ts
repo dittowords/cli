cb4196805bdcc294dfeca09b86ef0e41d6835966
Jordin Gardner <gard.jordin@gmail.com>
2023-10-27 11:53:16 -0700

    checkpoint

--- lib/utils/generateJsDriver.ts
+++ lib/utils/generateJsDriver.ts
@@ -4,6 +4,7 @@ import consts from "../consts";
 import output from "../output";
 import { Source } from "../types";
 import { cleanFileName } from "./cleanFileName";
+import { determineModuleType } from "./determineModuleType";
 
 // compatability with legacy method of specifying project ids
 // that is still used by the default format
@@ -22,18 +23,59 @@ const stringifySourceId = (projectId: string) =>
  * data from Ditto.
  */
 
-// TODO: support ESM
 export function generateJsDriver(sources: Source[]) {
-  const sourceIdsByName: Record<string, string> = sources.reduce(
-    (agg, source) => {
-      if (source.fileName) {
-        return { ...agg, [cleanFileName(source.fileName)]: source.id };
-      }
+  const moduleType = determineModuleType();
 
-      return agg;
-    },
-    {}
-  );
+  let filePath: string;
+  if (moduleType === "commonjs") {
+    filePath = generateJsDriverCommonJS(sources, moduleType);
+  } else if (moduleType === "module") {
+    filePath = generateJsDriverESM(sources);
+  } else {
+    throw new Error(`Unknown module type: ${moduleType}`);
+  }
+
+  return `Generated .js SDK driver at ${output.info(filePath)}`;
+}
+
+function generateJsDriverCommonJS(
+  sources: Source[],
+  moduleType: "commonjs" | "esm"
+) {
+  const variableNameGenerator = createVariableNameGenerator();
+  const importStatements: string[] = [];
+
+  const sourceInfoByName: Record<
+    string,
+    {
+      projectId: string;
+      variableName: string;
+      importStatement: string;
+      requireStatement: string;
+    }
+  > = {};
+
+  const data2: DriverFile = {};
+
+  sources.forEach((source) => {
+    if (!source.fileName) {
+      return;
+    }
+
+    const fileName = cleanFileName(source.fileName);
+    console.log("source.fileName", source.fileName);
+    const variableName = variableNameGenerator.generate(fileName.split(".")[0]);
+
+    const importStatement = `import ${variableName} from './${source.fileName}';`;
+    const requireStatement = `const ${variableName} = require('./${source.fileName}');`;
+
+    sourceInfoByName[fileName] = {
+      projectId: source.id,
+      variableName,
+      importStatement,
+      requireStatement,
+    };
+  });
 
   const projectFileNames = fs
     .readdirSync(consts.TEXT_DIR)
@@ -47,8 +89,8 @@ export function generateJsDriver(sources: Source[]) {
       const [sourceId, rest] = fileName.split("__");
       const [variantApiId] = rest.split(".");
 
-      const projectId = sourceIdsByName[sourceId];
-      const projectIdStr = stringifySourceId(projectId);
+      const sourceInfo = sourceInfoByName[sourceId];
+      const projectIdStr = stringifySourceId(sourceInfo.projectId);
 
       if (!obj[projectIdStr]) {
         obj[projectIdStr] = {};
@@ -59,6 +101,8 @@ export function generateJsDriver(sources: Source[]) {
     },
     {}
   );
+
+  console.log("data", data);
 
   // Create arrays of stringified "...require()" statements,
   // each of which corresponds to one of the component files
@@ -88,7 +132,24 @@ export function generateJsDriver(sources: Source[]) {
     data.ditto_component_library[key] = str;
   });
 
-  let dataString = `module.exports = ${JSON.stringify(data, null, 2)}`
+  let dataString = "";
+
+  Object.values(sourceInfoByName).forEach((sourceInfo) => {
+    if (moduleType === "commonjs") {
+      dataString += `${sourceInfo.requireStatement}\n`;
+      return;
+    }
+    if (moduleType === "esm") {
+      dataString += `${sourceInfo.importStatement}\n`;
+      return;
+    }
+    throw new Error("Unknown module type: " + moduleType);
+  });
+
+  dataString += "\n";
+  dataString += `${getExportPrefix(moduleType)}
+
+  dataString += ${JSON.stringify(data, null, 2)}`
     // remove quotes around require statements
     .replace(/"require\((.*)\)"/g, "require($1)")
     // remove quotes around opening & closing curlies
@@ -98,5 +159,37 @@ export function generateJsDriver(sources: Source[]) {
   const filePath = path.resolve(consts.TEXT_DIR, "index.js");
   fs.writeFileSync(filePath, dataString, { encoding: "utf8" });
 
-  return `Generated .js SDK driver at ${output.info(filePath)}`;
+  return filePath;
+}
+
+function createVariableNameGenerator() {
+  const variableNames = new Set<string>();
+
+  return {
+    generate: (str: string) => {
+      const baseName = str.replace(/\W/g, "_");
+      let name = baseName;
+      let i = 1;
+      while (variableNames.has(name)) {
+        name = `${baseName}${i}`;
+        i++;
+      }
+      variableNames.add(name);
+      return name;
+    },
+  };
+}
+
+function generateJsDriverESM(sources: Source[]) {
+  return "";
+}
+
+function getExportPrefix(moduleType: "commonjs" | "esm") {
+  if (moduleType === "commonjs") {
+    return "module.exports = ";
+  }
+  if (moduleType === "esm") {
+    return "export default ";
+  }
+  throw new Error("Unknown module type: " + moduleType);
 }

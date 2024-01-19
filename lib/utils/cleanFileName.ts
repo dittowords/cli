8e5c876afddc68c119cf57e78750b259b7dbd68f
Reed Barnes <reed@dittowords.com>
2024-01-19 14:43:00 -0800

    DIT-5759: component folder dev IDs properly exclude emojis

--- lib/utils/cleanFileName.ts
+++ lib/utils/cleanFileName.ts
@@ -1,6 +1,8 @@
 export function cleanFileName(fileName: string): string {
-  return fileName
-    .replace(/\s{1,}/g, "-")
-    .replace(/[^a-zA-Z0-9-_.]/g, "")
-    .toLowerCase();
+  let parts = fileName.split(/\s{1,}/g);
+  parts = parts.map((part) =>
+    part.replace(/[^a-zA-Z0-9-_.]/g, "").toLowerCase()
+  );
+  parts = parts.filter((part) => part !== "");
+  return parts.join("-");
 }

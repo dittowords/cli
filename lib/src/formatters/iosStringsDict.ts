ec44b6840c5250fe2d1476b417824974f8384ea8
Brian Parrish <brian.parrish3739@gmail.com>
2025-12-15 10:36:44 -0500

    Refactor IOSStringsFileFormatter into BaseExportFormatter to be shared amongst all export formats. Updated IOSStringsDict to use that class

--- lib/src/formatters/iosStringsDict.ts
+++ lib/src/formatters/iosStringsDict.ts
@@ -1,142 +1,21 @@
-import fetchText from "../http/textItems";
-import { ExportComponentsResponse, ExportTextItemsResponse, PullQueryParams } from "../http/types";
-import fetchComponents from "../http/components";
-import BaseFormatter from "./shared/base";
-import { applyMixins } from "./shared";
-import fetchProjects from "../http/projects";
-import fetchVariants from "../http/variants";
+import { PullQueryParams } from "../http/types";
 import IOSStringsDictOutputFile from "./shared/fileTypes/IOSStringsDictOutputFile";
+import BaseExportFormatter from "./shared/baseExport";
+export default class IOSStringsDictFormatter extends BaseExportFormatter<
+  IOSStringsDictOutputFile<{ variantId: string }>
+> {
+  public exportFormat: PullQueryParams["format"] = "ios-stringsdict";
 
-interface ComponentsMap {
-  [variantId: string]: ExportComponentsResponse
-}
-interface TextItemsMap {
-  [projectId: string]: {
-    [variantId: string]: ExportTextItemsResponse
-  }
-}
-
-type IOSStringsDictAPIData = {
-  textItemsMap: TextItemsMap;
-  componentsMap: ComponentsMap;
-};
-
-export default class IOSStringsDictFormatter extends applyMixins(
-  BaseFormatter<IOSStringsDictOutputFile<{ variantId: string }>, IOSStringsDictAPIData>) {
-  private variants: { id: string }[] = [];
-
-  protected async fetchAPIData() {
-    await this.fetchVariants();
-    const textItemsMap = await this.fetchTextItemsMap();
-    const componentsMap = await this.fetchComponentsMap();
-
-    return { textItemsMap, componentsMap };
-  }
-
-  /**
-   * For each project/variant permutation and its fetched .strings data,
-   * create a new file with the expected naming
-   * 
-   * @returns {OutputFile[]} List of Output Files
-   */
-  protected transformAPIData(data: IOSStringsDictAPIData) {
-    Object.entries(data.textItemsMap).forEach(([projectId, projectVariants]) => {
-      Object.entries(projectVariants).forEach(([variantId, iosStringsDictFile]) => {
-        const fileName = `${projectId}___${variantId || "base"}`;
-        this.outputFiles[fileName] ??= new IOSStringsDictOutputFile({
-          filename: fileName,
-          path: this.outDir,
-          metadata: { variantId: variantId || "base" },
-          content: iosStringsDictFile
-        });
-      });
+  protected createOutputFile(
+    fileName: string,
+    variantId: string,
+    content: string
+  ): void {
+    this.outputFiles[fileName] ??= new IOSStringsDictOutputFile({
+      filename: fileName,
+      path: this.outDir,
+      metadata: { variantId: variantId || "base" },
+      content: content,
     });
-
-    Object.entries(data.componentsMap).forEach(([variantId, iosStringsDictFile]) => {
-      const fileName = `components___${variantId || "base"}`;
-      this.outputFiles[fileName] ??= new IOSStringsDictOutputFile({
-        filename: fileName,
-        path: this.outDir,
-        metadata: { variantId: variantId || "base" },
-        content: iosStringsDictFile
-      });
-    })
-
-    return Object.values(this.outputFiles);
-  }
-
-  /**
-   * Sets variants based on configuration
-   * - Fetches from API if "all" configured
-   * - Adds "base" variant by default if none configured
-   */
-  private async fetchVariants(): Promise<void> {
-    let variants: { id: string }[] = this.output.variants ?? this.projectConfig.variants ?? [];
-    if (variants.some((variant) => variant.id === 'all')) {
-      variants = await fetchVariants(this.meta);
-    } else if (variants.length === 0) {
-      variants = [{ id: 'base' }]
-    }
-
-    this.variants = variants;
-  }
-
-  /**
-   * Fetches text item data via API for each configured project and variant
-   * in this output
-   * 
-   * @returns text items mapped to their respective variant and project
-   */
-  private async fetchTextItemsMap(): Promise<TextItemsMap> {
-    if (!this.projectConfig.projects && !this.output.projects) return {};
-    let projects: { id: string }[] = this.output.projects ?? this.projectConfig.projects ?? [];
-
-    const result: TextItemsMap = {};
-
-    if (projects.length === 0) {
-      projects = await fetchProjects(this.meta);
-    }
-
-    for (const project of projects) {
-      result[project.id] = {};
-
-      for (const variant of this.variants) {
-        // map "base" to undefined, as by default export endpoint returns base variant
-        const variantsParam = variant.id === 'base' ? undefined : [{ id: variant.id }]
-        const params: PullQueryParams = { 
-          ...super.generateQueryParams("textItem", { projects: [{ id: project.id }], variants: variantsParam }),
-          format: 'ios-stringsdict'
-        };
-        const iosStringsFile = await fetchText<ExportTextItemsResponse>(params, this.meta);
-        result[project.id][variant.id] = iosStringsFile;
-      }
-    }
-    
-    return result;
-  }
-
-  /**
-   * Fetches component data via API.
-   * If individual variants configured, fetch by each otherwise fetch for all
-   * Skips the fetch request if components field is not specified in config.
-   * 
-   * @returns components data
-   */
-  private async fetchComponentsMap(): Promise<ComponentsMap> {
-    if (!this.projectConfig.components && !this.output.components) return {};
-    const result: ComponentsMap = {};
-
-    for (const variant of this.variants) {
-      // map "base" to undefined, as by default export endpoint returns base variant
-      const variantsParam = variant.id === 'base' ? undefined : [{ id: variant.id }]
-      const params: PullQueryParams = { 
-        ...super.generateQueryParams("component", { variants: variantsParam }),
-        format: 'ios-stringsdict'
-      };
-      const iosStringsFile = await fetchComponents<ExportComponentsResponse>(params, this.meta);
-      result[variant.id] = iosStringsFile;
-    }
-
-    return result;
   }
 }
---
+++ lib/src/formatters/shared/baseExport.ts
@@ -0,0 +1,198 @@
+import fetchText from "../../http/textItems";
+import {
+  ExportComponentsResponse,
+  ExportTextItemsResponse,
+  PullQueryParams,
+} from "../../http/types";
+import fetchComponents from "../../http/components";
+import BaseFormatter from "./base";
+import fetchProjects from "../../http/projects";
+import fetchVariants from "../../http/variants";
+import OutputFile from "./fileTypes/OutputFile";
+
+interface ComponentsMap {
+  [variantId: string]: ExportComponentsResponse;
+}
+interface TextItemsMap {
+  [projectId: string]: {
+    [variantId: string]: ExportTextItemsResponse;
+  };
+}
+
+type ExportFormatAPIData = {
+  textItemsMap: TextItemsMap;
+  componentsMap: ComponentsMap;
+};
+
+type ExportOutputFile<MetadataType extends { variantId: string }> = OutputFile<
+  string,
+  MetadataType
+>;
+
+export default class BaseExportFormatter<
+  TOutputFile extends ExportOutputFile<{ variantId: string }>
+> extends BaseFormatter<TOutputFile, ExportFormatAPIData> {
+  public exportFormat: PullQueryParams["format"];
+  private variants: { id: string }[] = [];
+
+  protected createOutputFile(
+    fileName: string,
+    variantId: string,
+    content: string
+  ): void {}
+
+  protected async fetchAPIData() {
+    await this.fetchVariants();
+    const textItemsMap = await this.fetchTextItemsMap();
+    const componentsMap = await this.fetchComponentsMap();
+
+    return { textItemsMap, componentsMap };
+  }
+
+  /**
+   * For each project/variant permutation and its fetched .strings data,
+   * create a new file with the expected naming
+   *
+   * @returns {OutputFile[]} List of Output Files
+   */
+  protected transformAPIData(data: ExportFormatAPIData): TOutputFile[] {
+    Object.entries(data.textItemsMap).forEach(
+      ([projectId, projectVariants]) => {
+        Object.entries(projectVariants).forEach(
+          ([variantId, iosStringsFile]) => {
+            const fileName = `${projectId}___${variantId || "base"}`;
+            this.createOutputFile(fileName, variantId, iosStringsFile);
+          }
+        );
+      }
+    );
+
+    Object.entries(data.componentsMap).forEach(
+      ([variantId, iosStringsFile]) => {
+        const fileName = `components___${variantId || "base"}`;
+        this.createOutputFile(fileName, variantId, iosStringsFile);
+      }
+    );
+
+    return Object.values(this.outputFiles);
+  }
+
+  /**
+   * Sets variants based on configuration
+   * - Fetches from API if "all" configured
+   * - Adds "base" variant by default if none configured
+   */
+  private async fetchVariants(): Promise<void> {
+    let variants: { id: string }[] =
+      this.output.variants ?? this.projectConfig.variants ?? [];
+    if (variants.some((variant) => variant.id === "all")) {
+      variants = await fetchVariants(this.meta);
+    } else if (variants.length === 0) {
+      variants = [{ id: "base" }];
+    }
+
+    this.variants = variants;
+  }
+
+  /**
+   * Fetches text item data via API for each configured project and variant
+   * in this output
+   *
+   * @returns text items mapped to their respective variant and project
+   */
+  private async fetchTextItemsMap(): Promise<TextItemsMap> {
+    if (!this.projectConfig.projects && !this.output.projects) return {};
+    let projects: { id: string }[] =
+      this.output.projects ?? this.projectConfig.projects ?? [];
+
+    const result: TextItemsMap = {};
+
+    if (projects.length === 0) {
+      projects = await fetchProjects(this.meta);
+    }
+
+    for (const project of projects) {
+      result[project.id] = {};
+
+      for (const variant of this.variants) {
+        // map "base" to undefined, as by default export endpoint returns base variant
+        const variantsParam =
+          variant.id === "base" ? undefined : [{ id: variant.id }];
+        const params: PullQueryParams = {
+          ...super.generateQueryParams("textItem", {
+            projects: [{ id: project.id }],
+            variants: variantsParam,
+          }),
+          format: this.exportFormat,
+        };
+        const iosStringsFile = await fetchText<ExportTextItemsResponse>(
+          params,
+          this.meta
+        );
+        result[project.id][variant.id] = iosStringsFile;
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * Fetches component data via API.
+   * If individual variants configured, fetch by each otherwise fetch for all
+   * Skips the fetch request if components field is not specified in config.
+   *
+   * @returns components data
+   */
+  private async fetchComponentsMap(): Promise<ComponentsMap> {
+    if (!this.projectConfig.components && !this.output.components) return {};
+    const result: ComponentsMap = {};
+
+    for (const variant of this.variants) {
+      // map "base" to undefined, as by default export endpoint returns base variant
+      const variantsParam =
+        variant.id === "base" ? undefined : [{ id: variant.id }];
+      const params: PullQueryParams = {
+        ...super.generateQueryParams("component", { variants: variantsParam }),
+        format: this.exportFormat,
+      };
+      const iosStringsFile = await fetchComponents<ExportComponentsResponse>(
+        params,
+        this.meta
+      );
+      result[variant.id] = iosStringsFile;
+    }
+
+    return result;
+  }
+}
+
+/*
+
+export default class BaseExportFormatter<TOutputFile extends OutputFile<unknown, unknown>> extends BaseFormatter<
+
+
+
+type ExportOutputFileConstructor<
+  MetadataType extends { variantId: string },
+  TOutputFile extends ExportOutputFile<MetadataType>
+> = new (config: {
+  filename: string;
+  path: string;
+  metadata: MetadataType;
+  content: string;
+}) => TOutputFile;
+
+  /**
+   * The export format to request from the API, e.g. "ios-strings" or "ios-stringsdict".
+   * Must be configured by subclasses.
+   */
+// protected abstract format: ExportFormat;
+
+/**
+   * The OutputFile constructor used when creating files in transformAPIData.
+   * Must be configured by subclasses.
+  // protected abstract OutputFileCtor: ExportOutputFileConstructor<
+  //   { variantId: string },
+  //   TOutputFile
+  // >;
+*/
